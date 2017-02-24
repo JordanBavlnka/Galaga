428022d9a993cbcd6a6a7f3b0458736898b659f6
18dcb01 <danielbarnett5000000@gmail.com>
2017-02-24 15:51:22 -0600

    Create Bee.cpp

---
+++ Bee.cpp
@@ -0,0 +1,24 @@
+#include "stdafx.h"
+#include "Bee.h"
+
+
+Bee::Bee()
+{
+}
+
+
+Bee::~Bee()
+{
+}
+
+
+void Bee::swoop()
+{
+	//swoop
+}
+
+
+void Bee::transform(int numBugs)
+{
+	//turn into bonus stuff
+}
