7f1aac746db6184724bd37dda274897334f9cb5c
18dcb01 <danielbarnett5000000@gmail.com>
2017-02-28 15:14:31 -0600

    Create Galaga.cpp

---
+++ Galaga.cpp
@@ -0,0 +1,37 @@
+#pragma once
+#include <vector>
+#include "Galaga.h"
+
+Galaga::Galaga()
+{
+}
+
+
+Galaga::~Galaga()
+{
+}
+
+
+void Galaga::play()
+{
+}
+
+
+void Galaga::checkCollision()
+{
+}
+
+
+void Galaga::drawScore()
+{
+}
+
+
+void Galaga::drawLevel()
+{
+}
+
+
+void Galaga::render()
+{
+}
