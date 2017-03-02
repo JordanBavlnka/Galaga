6ace83ea57eec8622ab9403c295cb6d05ccc91d4
18dcb01 <danielbarnett5000000@gmail.com>
2017-03-02 15:49:10 -0600

    Update Galaga.cpp

--- Galaga.cpp
+++ Galaga.cpp
@@ -14,6 +14,29 @@ Galaga::~Galaga()
 
 void Galaga::play()
 {
+  bool quit;
+  do 
+  {
+    for(int i = 0; i<something i++)
+    {
+      //carry menu up
+    }
+    
+    do 
+    {
+    //display menu
+    }while(!clickedOnAnything);
+    
+    do
+    {
+      bool enemiesAlive;
+      do{
+        //stuff
+        
+        
+      }while(enemies.size()>0)
+    }while(lives>0);
+  }while(!quit)
 }
 
 
